16aafb185ae9ebdc22a91a8d9bbdc294fe20b51f
SapozhnikovAndreyV <sapozhnikov.andrey.v@yandex.ru>
2021-03-15 21:41:29 +0300

    constAndVariab

---
+++ ConstantsAndVariables.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+let maximumNumbersOfLoginAttempts=10
+var currentLoginAttempt=0
+var welcomeMessage: String
+welcomeMessage = "Hello"
+let languageName="Swift"
+print(welcomeMessage,terminator:"")
+let dog="!";print(dog)
+let minValue=UInt8.max
+print(minValue)
